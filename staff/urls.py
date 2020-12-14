2b4daec9f0149201717ce9c58c68463ae41a37eb
Sujena <sujenasb@gmail.com>
2020-12-14 15:04:45 +0545

    Initial Commit - Added Login Page

---
+++ staff/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='staff-home'),
+    path('login/', auth_views.LoginView.as_view(template_name='staff/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='staff/logout.html'), name='logout'),
+]
